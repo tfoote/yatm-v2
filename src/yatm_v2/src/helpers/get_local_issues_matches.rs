445a895c5d3851ca2075f977fc7aa084667e2ea7
Tully Foote <tullyfoote@intrinsic.ai>
2025-06-29 00:18:28 -0700

    first attempts at testing new reentrant mechanisms

--- src/yatm_v2/src/helpers/get_local_issues_matches.rs
+++ src/yatm_v2/src/helpers/get_local_issues_matches.rs
@@ -86,3 +86,250 @@ fn is_local_issue_identical_github_issue(
     // Full match
     title_match && body_match
 }
+
+#[cfg(test)]
+mod test_get_local_issues_matches {
+    use crate::helpers::get_local_issues_matches;
+    use crate::types::LocalIssue;
+    use octocrab::models::issues::Issue as GithubIssue;
+
+    // use super::get_local_issues_matches;
+    use super::{IssueMatchType, GithubIssueMatches};
+
+    #[test]
+    fn matches() {
+        let local_issues = vec![
+            LocalIssue {
+                labels: vec!["label1".to_string()],
+                title: "title".to_string(),
+                text_body: "text_body".to_string(),
+            },
+            LocalIssue {
+                labels: vec!["label2".to_string()],
+                title: "title2".to_string(),
+                text_body: "text_body2".to_string(),
+            },
+            LocalIssue {
+                labels: vec!["label3".to_string()],
+                title: "identical".to_string(),
+                text_body: "text_body3".to_string(),
+            },
+        ];
+        let github_issues: Vec<_> = vec![
+            GithubIssue {
+                labels: vec![String::from("label3")],
+                title: "identical".to_string(),
+                body: Some("text_body3".to_string()),
+            },
+            GithubIssue {
+                labels: vec!["label4".to_string()],
+                title: "Extra".to_string(),
+                body: Some("".to_string()),
+            },
+        ];
+        let result = get_local_issues_matches(&local_issues, &github_issues);
+
+        let missing_count: i32 = 0;
+        let match_count: i32 = 0;
+        let match_diff_count: i32 = 0;
+
+
+        for imatch in result.iter() {
+            match imatch.match_type {
+                IssueMatchType::Match => match_count += 1,
+                IssueMatchType::MatchedWithDiff => match_diff_count +=1,
+                IssueMatchType::Missing => missing_count +=1,
+            }
+        }
+        assert_eq!(missing_count, 1);
+        assert_eq!(match_count, 1);
+        assert_eq!(match_diff_count, 1);
+    }
+}
+//     #[test]
+//     fn no_matches() {
+//         let local_issues = vec![
+//             LocalIssue {
+//                 labels: vec!["label1".to_string()],
+//                 title: "title".to_string(),
+//                 text_body: "text_body".to_string(),
+//             },
+//             LocalIssue {
+//                 labels: vec!["label2".to_string()],
+//                 title: "title2".to_string(),
+//                 text_body: "text_body2".to_string(),
+//             },
+//         ];
+//         let github_issues: Vec<_> = vec![
+//             GithubIssueHelper {
+//                 labels: vec!["label1".to_string()],
+//                 title: "title".to_string(),
+//             },
+//             GithubIssueHelper {
+//                 labels: vec!["label2".to_string()],
+//                 title: "title2".to_string(),
+//             },
+//         ];
+//         let result = get_local_issues_without_matches_helper(&local_issues, &github_issues);
+//         assert_eq!(result.len(), 0);
+//     }
+
+//     #[test]
+//     fn one_match() {
+//         let local_issues = vec![
+//             LocalIssue {
+//                 labels: vec!["label1".to_string()],
+//                 title: "title".to_string(),
+//                 text_body: "text_body".to_string(),
+//             },
+//             LocalIssue {
+//                 labels: vec!["label2".to_string()],
+//                 title: "title2".to_string(),
+//                 text_body: "text_body2".to_string(),
+//             },
+//         ];
+//         let github_issues: Vec<_> = vec![
+//             GithubIssueHelper {
+//                 labels: vec!["label1".to_string()],
+//                 title: "title".to_string(),
+//             },
+//             GithubIssueHelper {
+//                 labels: vec!["label3".to_string()],
+//                 title: "title3".to_string(),
+//             },
+//         ];
+//         let result = get_local_issues_without_matches_helper(&local_issues, &github_issues);
+//         assert_eq!(result.len(), 1);
+//         assert_eq!(result[0].title, "title2");
+//     }
+// }
+
+// fn is_local_issue_match_github_issue_helper(
+//     local_issue: &LocalIssue,
+//     github_issue: &GithubIssueHelper,
+// ) -> bool {
+//     if local_issue.title != github_issue.title {
+//         return false;
+//     }
+//     if local_issue.labels.len() > github_issue.labels.len() {
+//         return false;
+//     }
+//     for label in local_issue.labels.iter() {
+//         if !github_issue
+//             .labels
+//             .iter()
+//             .any(|github_label| github_label == label)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// #[cfg(test)]
+// mod test_is_local_issue_match_github_issue {
+//     use super::is_local_issue_match_github_issue_helper;
+//     use super::{GithubIssueHelper, LocalIssue};
+
+//     #[test]
+//     fn is_match() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//         };
+//         assert!(is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+
+//     #[test]
+//     fn is_match_with_github_issue_having_more_labels() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label1".to_string(), "label2".to_string()],
+//             title: "title".to_string(),
+//         };
+//         assert!(is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+
+//     #[test]
+//     fn is_not_match_label() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label2".to_string()],
+//             title: "title".to_string(),
+//         };
+//         assert!(!is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+
+//     #[test]
+//     fn is_not_match_title() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label1".to_string()],
+//             title: "title2".to_string(),
+//         };
+//         assert!(!is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+
+//     #[test]
+//     fn is_not_match_label_and_title() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label2".to_string()],
+//             title: "title2".to_string(),
+//         };
+//         assert!(!is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+
+//     #[test]
+//     fn is_not_match_missing_label() {
+//         let local_issue = LocalIssue {
+//             labels: vec!["label1".to_string(), "label2".to_string()],
+//             title: "title".to_string(),
+//             text_body: "text_body".to_string(),
+//         };
+//         let github_issue = GithubIssueHelper {
+//             labels: vec!["label2".to_string()],
+//             title: "title".to_string(),
+//         };
+//         assert!(!is_local_issue_match_github_issue_helper(
+//             &local_issue,
+//             &github_issue
+//         ));
+//     }
+// }
